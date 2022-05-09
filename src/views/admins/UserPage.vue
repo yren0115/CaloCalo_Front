ea52fc268bdefbc0679016725e26e71b584d71b5
Ren Yoshizawa <renyoshizawa0515@gmail.com>
2022-05-09 09:14:06 +0900

    update setting function

---
+++ src/views/admins/UserPage.vue
@@ -0,0 +1,171 @@
+<template>
+   <v-app>
+    <v-app-bar app color="blue darken-4" class="white--text" clipped-left>
+      <v-app-bar-nav-icon app color="white" v-on:click="drawer=!drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="header-bar">CaloCalo</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn v-on:click="logout" color="red" class="white--text" >ログアウト</v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer app v-model="drawer" color="black" clipped>
+      <div class="top-title">Menu</div>
+      <v-list>
+
+        <v-row class="vertical-spacer" justify="center">
+          <v-btn large class="menu-btn" v-on:click="subst0"><v-icon>mdi-human-handsdown</v-icon>MyPage</v-btn>
+        </v-row>
+         <v-row class="vertical-spacer" justify="center">
+             <v-btn large class="menu-btn" v-on:click="subst1"><v-icon>mdi-file </v-icon>Log</v-btn>
+        </v-row>
+        <v-row class="vertical-spacer" justify="center">
+          <v-btn large class="menu-btn" v-on:click="subst2"><v-icon>mdi-cog</v-icon>Setting</v-btn>
+        </v-row>
+
+      </v-list>
+    </v-navigation-drawer>
+
+    <!-- v-mainはデフォルトで余白やその他スタイルが自動指定されてる -->
+    <v-main class="pt-0 blue-grey lighten-4">
+
+      <router-view>
+        <div v-if="menuflag == 0" class="menu-bif">
+          <UserTop />
+        </div>
+        <div v-if="menuflag == 1" class="menu-bif">
+          <UserLog />
+        </div>
+        <div v-if="menuflag == 2" class="menu-bif">
+          <UserSet />
+        </div>
+      </router-view>
+
+    </v-main>
+
+    <v-footer class="blue darken-4" dark app>
+      <p class="reset-css footer-layout">Copyright © 2022 SBチームA. All Rights</p>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import UserTop from '@/views/users/UserTop.vue'
+import UserLog from '@/views/users/UserLog.vue'
+import UserSet from '@/views/users/UserSet.vue'
+
+export default {
+  name: 'MyPage',
+  components: {
+    UserTop,
+    UserLog,
+    UserSet,
+  },
+  data: ()=> ({
+    drawer: null,
+    user: {},
+    menuflag: 0,
+  }),
+   computed: {
+    getintakeCalorie: function() {
+      return this.$store.state.intakeCalorie;
+    },
+    getgoalCalorie: function() {
+      return this.$store.state.goalCalorie;
+    }
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch("auth", {
+        empId: '0',
+        password: 'qazplm',
+      });
+      this.$router.push('/login')
+    },
+    toUserLog() {
+      this.$router.push('/userlog')
+    },
+    subst0: function() {
+      this.menuflag = 0;
+      this.$router.push('/usertop')
+    },
+    subst1: function() {
+      this.menuflag = 1;
+      this.$router.push('/userlog')
+    },
+    subst2: function() {
+      this.menuflag = 2;
+      this.$router.push('/userset')
+    },
+    submit() {
+      this.$store.dispatch("setcalo", {
+        intakeCalorie: this.user.intakeCalorie,
+      });
+    },
+    clear() {
+      this.$refs.form.reset();
+    },
+  },
+};
+</script>
+
+<style scoped>
+.reset-css {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.top-title {
+  color: white;
+  text-align: center;
+  font-size: 30px;
+  padding: 10px 0 60px 0;
+}
+.logout-btn {
+  color: white;
+  font-size: 20px;
+}
+.vertical-spacer {
+  padding-bottom: 60px;
+}
+.menu-btn {
+  margin: 0 auto;
+}
+.left-title {
+  color: black;
+  text-align: center;
+  padding: 100px 0 50px;
+}
+.left-title-sub {
+  color: black;
+  text-align: center;
+  padding: 50px 0;
+}
+.form-container {
+  height: 90vh;
+  width: 100%;
+}
+.btn-container {
+  margin: 0 auto;
+}
+.output-container {
+  height: 90vh;
+  width: 100%;
+}
+.right-title {
+  color: black;
+  text-align: center;
+  padding: 100px 0 50px;
+}
+.right-title-sub {
+  color: black;
+  text-align: center;
+  padding: 115px 0 50px;
+}
+.goal-cal-disp {
+  text-align: center;
+  line-height: 150px;
+}
+.footer-layout {
+  width: 100%;
+  text-align: center;
+}
+</style>
--- src/views/users/UserSet.vue
+++ src/views/users/UserSet.vue
@@ -12,12 +12,17 @@
             <div class="lower-container">
               <v-label><h2 class="lower-title">目標カロリーを入力してください</h2></v-label>
               <v-form ref="form">
-                <v-textarea solo height="200px" name="input-7-4" label="目標カロリーを入力" v-model="user.goalCalorie">
+
+                <v-textarea :rules="inputRules" solo height="200px" name="input-7-4" label="目標カロリーを入力" @input="validate" v-model.number="user.goalCalorie">
                 </v-textarea>
+
                 <div class="btn-container">
-                  <v-btn class="mr-4" v-on:click="submitGoalCalo(); clear()">SUBMIT</v-btn>
+                  <v-btn class="mr-4" v-on:click="postGoalCalorie(); clear()">SUBMIT</v-btn>
                 </div>
               </v-form>
+               <h3>入力された目標カロリー:{{ user.goalCalorie }}</h3>
+               <h3>非同期で取得したデータ:{{ title }}</h3>
+               <h3>{{ goalCalorie }}</h3>
             </div>
           </v-col>
       </v-row>
@@ -25,6 +30,18 @@
 </template>
 
 <script>
+// const SETTING_URL = '/mypage/setting/';
+// const DOMAINE = 'http://localhost:80/';
+// const CONTEXT_PATH = "calocalo/";
+
+import axios from "axios";
+
+// const BASE_URL = DOMAINE + CONTEXT_PATH;
+// BASE_URL + SETTING_URL + {emp_id}
+
+const URL = "http://localhost:3000/goalcalories/1";
+// const EMPID = this.getempId;
+// const PUT_URL = URL + EMPID;
 
 export default ({
   name: 'UserSet',
@@ -33,13 +50,23 @@ export default ({
   data: ()=> ({
     drawer: null,
     user: {},
+    numValue: 0,
     menuflag: 0,
+    goalCalorie: 0,
+    title: "",
+    inputRules: [
+      v => !!v || '0以上の数値で入力してください',
+      v => v.length <= 4 || '9999までの数値で入力してください',
+    ],
   }),
    computed: {
+     getempId: function() {
+       return this.$store.state.empId;
+     },
     getintakeCalorie: function() {
       return this.$store.state.intakeCalorie;
     },
-    getoalCalorie: function() {
+    getgoalCalorie: function() {
       return this.$store.state.goalCalorie;
     },
   },
@@ -54,10 +81,27 @@ export default ({
         goalCalorie: this.user.goalCalorie,
       });
     },
+    validate() {
+      this.numValue = this.numValue.replace(/\D/g, '')
+    }, 
     clear() {
       this.$refs.form.reset();
     },
+
+    postGoalCalorie() {
+      // var rempId = this.getempId
+      axios
+      .put((URL), {
+        id: this.getempId,
+        goalcalorie: this.user.goalCalorie
+      })
+    },
   },
+  watch: {
+      getgoalCalorie: function() {
+      this.goalCalorie()
+    },
+  }
 });
 </script>
 
@@ -84,9 +128,6 @@ export default ({
   text-align: center;
   padding: 50px 0;
 }
-
-
-
 .set-title {
   margin-top: 100px;
 }
