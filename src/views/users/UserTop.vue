7fa10fb3431138a984a946f440bc3ac082ef7023
ito-yo <ito-yo@softbrain.co.jp>
2022-05-13 11:01:01 +0900

    order change function added: work properly

--- src/views/users/UserTop.vue
+++ src/views/users/UserTop.vue
@@ -5,11 +5,14 @@
           <div class="form-container">
             <v-form ref="form">
             <v-label><h2 class="left-title">本日の摂取カロリー入力欄</h2></v-label>
-              <v-select :items="foodList" label="摂取した食品を選択してください" dense outlined v-model="selectedFood">
-              </v-select>
+              <!-- <v-select :items="foodList" label="摂取した食品を選択してください" dense outlined v-model="selectedFood"> -->
+              <div v-if="oName == orderWatcher"><v-select  :items="foodNameOrderList" label="摂取した食品を選択してください" dense outlined v-model="selectedFood"></v-select></div>
+              <div v-else-if="oMax == orderWatcher"><v-select  :items="foodCaloMaxOrderList" label="摂取した食品を選択してください" dense outlined v-model="selectedFood"></v-select></div>
+              <div v-else-if="oMin == orderWatcher"><v-select :items="foodCaloMinOrderList" label="摂取した食品を選択してください" dense outlined v-model="selectedFood"></v-select></div>
                 <div class="btn-container">
                   <!-- <v-btn class="mr-4" :disabled="processing" v-on:click.prevent="submitUpdateCalorie" >submit</v-btn> -->
                   <v-btn class="mr-4" v-on:click.prevent="submitUpdateCalorie" >submit</v-btn>
+                  <v-btn v-on:click="changeOrder">{{ switchName }}</v-btn>
                 </div>
               </v-form>
               <v-label><h2 class="left-title-sub">本日の摂取カロリー</h2></v-label>
@@ -41,6 +44,8 @@
 </template>
 
 <script>
+import axios from "axios";
+
 const PROTOCOLE = 'http://'
 const DOMAINE = 'localhost';
 const PORT = ':8000/'
@@ -51,8 +56,10 @@ const FOODS_URL= `food_list/`;
 const EMP_GOAL_URL = `employee/info/`;
 const EMP_INTAKE_CALO_URL = `employee/take_calorie/`;
 const EMP_SUBMIT_RECORD_URL = 'submit/food/'
-import axios from "axios";
 
+      const NAME = 1;
+      const MAX = 2;
+      const MIN = 3;
 export default {
   name: 'UserTop',
 
@@ -68,6 +75,14 @@ export default {
     user: {},
     menuflag: 0,
     foodList:[],
+    foodNameOrderList:[],
+    foodCaloMaxOrderList:[],
+    foodCaloMinOrderList:[],
+    oName:NAME,
+    oMax:MAX,
+    oMin:MIN,
+    order:1,
+
     foodObjectList:{},
     selectedFoodId:null,
     foodName:null,
@@ -75,30 +90,76 @@ export default {
     goalCalorie:0,
     calorieToday:0,
     selectedFood:null,
-    disabledButton:true
+    disabledButton:true,
+
+    nameOrderList:null,
+    caloMaxOrder:null,
+    caloMinOrder:null
+
     // processing:false 
 
   }),
 
    computed: {
+    orderWatcher: function (){return this.order},
     calorieAbailable: function() {
       return this.goalCalorie - this.calorieToday;
     },
     intakeCalorieToday: function() {
       return this.calorieToday;
+    },
+    switchName: function(){
+      var buttonLabel;
+      var orderNum = this.order;
+
+
+      switch (orderNum){
+        case NAME:
+        buttonLabel = 'Name';
+        break;
+        case MAX:
+        buttonLabel = ' MAX';
+        break;
+        case MIN:
+        buttonLabel = 'MINI';
+        break;
+      }
+      return buttonLabel;
     }
   },
 
   methods: {
+
+    changeOrder(){
+      var orderNum = this.order;
+      orderNum = orderNum >= 3 ? 0 : orderNum;
+      this.order = ++orderNum;
+    },
 
     fetchFoodList() {
       var vm = this
       axios.get(BASE_URL + FOODS_URL)
       .then(function (response) {
         vm.foodObjectList = response.data.food_list;
-        for (var i = 0; i < vm.foodObjectList.length; i++) {
-          vm.foodList.push(vm.foodObjectList[i].food_name);
-        }
+        vm.createFoodNameList(vm.foodObjectList, vm.foodList);
+        var foodCaloAcceObjList = vm.duplicateSortFoodObjListByCalorieAccending(vm.foodObjectList);
+        vm.createFoodNameList(foodCaloAcceObjList, vm.foodCaloMinOrderList);
+        var foodCaloDecObjList = foodCaloAcceObjList.reverse();
+        vm.createFoodNameList(foodCaloDecObjList, vm.foodCaloMaxOrderList);
+        var foodNameAcceObjList = vm.duplicateSortFoodObjListByNameAccending(vm.foodObjectList);
+        vm.createFoodNameList(foodNameAcceObjList, vm.foodNameOrderList);
+// ############################# delete 
+        console.log(vm.foodCaloMaxOrderList);
+        console.log(vm.foodCaloMaxOrderList);
+
+        
+        
+        // vm.foodObjectList = response.data.food_list;
+        // for (var i = 0; i < vm.foodObjectList.length; i++) {
+        //   vm.foodList.push(vm.foodObjectList[i].food_name);
+        // }
+        // var caloAcceObjList = duplicateSortFoodObjListByCalorieAccending(vm.foodObjectList);
+        
       })
     },
 
@@ -147,8 +208,62 @@ export default {
         }
       })
    },
+   cloneFoodObjectList(foodObjectList){
+        let clonedObjList = new Array();
+        for (var i = 0; i < foodObjectList.length; i++){
+            let foodObject = {
+                food_id:foodObjectList[i].food_id,
+                food_name:foodObjectList[i].food_name,
+                calorie:foodObjectList[i].calorie
+            }
+            clonedObjList.push(foodObject);
+        }
+        return clonedObjList;
+    },
+
+        duplicateSortFoodObjListByCalorieAccending(srcList){
+        let dupObjList = this.cloneFoodObjectList(srcList); 
+        let tmp;
+        for (var i = 0; i < dupObjList.length - 1;i++){
+            for (var j = i + 1; j < srcList.length; j++){
+                if (dupObjList[i].calorie > dupObjList[j].calorie){
+                    tmp = dupObjList[i];
+                    dupObjList[i] = dupObjList[j];
+                    dupObjList[j] = tmp;
+                }
+            }
+        }
+        return dupObjList;
+    },
+        duplicateSortFoodObjListByNameAccending(srcList){
+        let dupObjList = this.cloneFoodObjectList(srcList); 
+        let tmp;
+        for (var i = 0; i < dupObjList.length - 1;i++){
+            for (var j = i + 1; j < dupObjList.length; j++){
+                if (dupObjList[i].name > dupObjList[j].name){
+                    tmp = dupObjList[i];
+                    dupObjList[i] = dupObjList[j];
+                    dupObjList[j] = tmp;
+                }
+            }
+        }
+        return dupObjList;
+    },
+
+      createFoodNameList(vueObjectList, vueFoodNameList){ 
+        for (var i = 0; i < vueObjectList.length; i++) {
+          vueFoodNameList.push(vueObjectList[i].food_name);
+        }
+    }
   }
 }
+
+
+
+
+
+
+
 </script>
 
 <style scoped>
