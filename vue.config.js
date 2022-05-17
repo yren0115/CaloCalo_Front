4e169a8579106e2d42ec1e2fe856c45e8732e224
yrenren0115 <64722674+Lotushass15@users.noreply.github.com>
2022-05-17 23:04:58 +0900

    Revert "setting production env"

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ],
-  devServer: {
-    proxy: {
-        '/api/': {
-            target: 'http://appalb-calocalo-579011708.ap-northeast-1.elb.amazonaws.com:8000'
-        }
-    }
-}
+  ]
 })
