11a76c6dfa2c91a2dcc28e824b8fb1c6821cc8ad
yrenren0115 <64722674+Lotushass15@users.noreply.github.com>
2022-05-17 23:05:15 +0900

    Revert "Revert "setting production env""

--- src/views/users/UserLog.vue
+++ src/views/users/UserLog.vue
@@ -43,7 +43,7 @@ const PROTOCOLE = 'http://'
 // const DOMAINE = 'localhost';
 const DOMAINE = 'appalb-calocalo-579011708.ap-northeast-1.elb.amazonaws.com';
 const PORT = ':8000/'
-const CONTEXT_PATH = "calocalo/";
+const CONTEXT_PATH = "api/calocalo/";
 const BASE_URL = PROTOCOLE + DOMAINE + PORT + CONTEXT_PATH;
 
 const EMP_GOAL_URL = `employee/info/`;
